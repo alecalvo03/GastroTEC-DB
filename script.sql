5cf0ce0a3cc269925b9f7384541d2caa0ac5f172
alecalvo03 <ale_calvo03@outlook.com>
2018-09-06 14:10:34 -0600

    Selec most frequent carrer

--- script.sql
+++ script.sql
@@ -3,28 +3,28 @@ PRAGMA foreign_keys = ON;
  * DataBase Tables Creation
  */
 
-CREATE TABLE Universidad (
+CREATE TABLE UNIVERSIDAD (
     IdUniversidad         INTEGER PRIMARY KEY AUTOINCREMENT,
     Nombre                VARCHAR(40),
     IdCalificacion        INTEGER,
-    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES Calificacion(IdCalificacion));
+    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES CALIFICACION(IdCalificacion));
 
-CREATE TABLE Calificacion (
+CREATE TABLE CALIFICACION (
     IdCalificacion        INTEGER PRIMARY KEY AUTOINCREMENT,
     UpVotes               INTEGER,
     DownVotes             INTEGER);
 
-CREATE TABLE Sede (
+CREATE TABLE SEDE (
     IdSede                INTEGER PRIMARY KEY AUTOINCREMENT,
     Nombre                VARCHAR(20),
     IdCalificacion        INTEGER,
     IdUbicacion           INTEGER,
     IdUniversidad         INTEGER,
-    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES Universidad(IdUniversidad),
-    CONSTRAINT fk_ubucacion FOREIGN KEY (IdUbicacion) REFERENCES Ubicacion(IdUbicacion),
-    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES Calificacion(IdCalificacion));
+    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES UNIVERSIDAD(IdUniversidad),
+    CONSTRAINT fk_ubucacion FOREIGN KEY (IdUbicacion) REFERENCES UBICACION(IdUbicacion),
+    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES CALIFICACION(IdCalificacion));
 
-CREATE TABLE Ubicacion (
+CREATE TABLE UBICACION (
     IdUbicacion           INTEGER PRIMARY KEY AUTOINCREMENT,
     Pais                  VARCHAR(15),
     Provincia             VARCHAR(15),
@@ -32,7 +32,7 @@ CREATE TABLE Ubicacion (
     Calle                 VARCHAR(20),
     Direccion             VARCHAR(50));
 
-CREATE TABLE Estudiante (
+CREATE TABLE ESTUDIANTE (
     IdEstudiante          INTEGER PRIMARY KEY AUTOINCREMENT,
     IdUniversidad         INTEGER,
     Carrera               VARCHAR(40),
@@ -40,128 +40,128 @@ CREATE TABLE Estudiante (
     LugarNacimiento       INTEGER,
     Nombre                VARCHAR(20),
     Apellidos             VARCHAR(25),
-    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES Universidad(IdUniversidad),
-    CONSTRAINT fk_ubucacion FOREIGN KEY (LugarNacimiento) REFERENCES Ubicacion(IdUbicacion));
+    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES UNIVERSIDAD(IdUniversidad),
+    CONSTRAINT fk_ubucacion FOREIGN KEY (LugarNacimiento) REFERENCES UBICACION(IdUbicacion));
 
-CREATE TABLE Restaurante (
+CREATE TABLE RESTAURANTE (
     IdRestaurante         INTEGER PRIMARY KEY AUTOINCREMENT,
     Nombre                VARCHAR(30),
     Horario               VARCHAR(11),
     IdSede                INTEGER,
     IdCalificacion        INTEGER,
-    CONSTRAINT fk_sede FOREIGN KEY (IdSede) REFERENCES Sede(IdSede),
-    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES Calificacion(IdCalificacion));
+    CONSTRAINT fk_sede FOREIGN KEY (IdSede) REFERENCES SEDE(IdSede),
+    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES CALIFICACION(IdCalificacion));
 
-CREATE TABLE Menu (
+CREATE TABLE MENU (
     IdMenu                INTEGER PRIMARY KEY AUTOINCREMENT,
     IdRestaurante         INTEGER,
     Horario               TIME,
     Fecha                 DATE,
-    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante(IdRestaurante));
+    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES RESTAURANTE(IdRestaurante));
 
-CREATE TABLE Platillo (
+CREATE TABLE PLATILLO (
     IdPlatillo            INTEGER PRIMARY KEY AUTOINCREMENT,
     Nombre                VARCHAR(25),
     Precio                INTEGER,
     IdCalificacion        INTEGER,
-    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES Calificacion(IdCalificacion));
+    CONSTRAINT fk_calificacion FOREIGN KEY (IdCalificacion) REFERENCES CALIFICACION(IdCalificacion));
 
-CREATE TABLE Voto (
+CREATE TABLE VOTO (
     IdVoto                INTEGER PRIMARY KEY AUTOINCREMENT,
     Hora                  TIME,
     Fecha                 DATE,
     Calificacion          INTEGER);
 
-CREATE TABLE VotoxUniversidad(
+CREATE TABLE VOTOXUNIVERSIDAD(
     IdUniversidad         INTEGER,
     IdEstudiante          INTEGER,
     IdVoto                INTEGER,
-    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES Universidad(IdUniversidad),
-    CONSTRAINT fk_estudiante FOREIGN KEY (IdEstudiante) REFERENCES Estudiante(IdEstudiante),
-    CONSTRAINT fk_voto FOREIGN KEY (IdVoto) REFERENCES Voto(IdVoto));
+    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES UNIVERSIDAD(IdUniversidad),
+    CONSTRAINT fk_estudiante FOREIGN KEY (IdEstudiante) REFERENCES ESTUDIANTE(IdEstudiante),
+    CONSTRAINT fk_voto FOREIGN KEY (IdVoto) REFERENCES VOTO(IdVoto));
 
-CREATE TABLE VotoxRestaurante(
-    IdUniversidad         INTEGER,
-    IdRestaurante         INTEGER,
-    IdEstudiante          INTEGER,
-    IdVoto                INTEGER,
-    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES Universidad(IdUniversidad),
-    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante(IdRestaurante),
-    CONSTRAINT fk_estudiante FOREIGN KEY (IdEstudiante) REFERENCES Estudiante(IdEstudiante),
-    CONSTRAINT fk_voto FOREIGN KEY (IdVoto) REFERENCES Voto(IdVoto));
-
-CREATE TABLE VotoxPlatillo(
+CREATE TABLE VOTOXRESTAURANTE(
     IdUniversidad         INTEGER,
     IdRestaurante         INTEGER,
-    IdPlatillo            INTEGER,
     IdEstudiante          INTEGER,
     IdVoto                INTEGER,
-    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES Universidad(IdUniversidad),
-    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante(IdRestaurante),
-    CONSTRAINT fk_platillo FOREIGN KEY (IdPlatillo) REFERENCES Platillo(IdPlatillo),
-    CONSTRAINT fk_estudiante FOREIGN KEY (IdEstudiante) REFERENCES Estudiante(IdEstudiante),
-    CONSTRAINT fk_voto FOREIGN KEY (IdVoto) REFERENCES Voto(IdVoto));
+    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES UNIVERSIDAD(IdUniversidad),
+    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES RESTAURANTE(IdRestaurante),
+    CONSTRAINT fk_estudiante FOREIGN KEY (IdEstudiante) REFERENCES ESTUDIANTE(IdEstudiante),
+    CONSTRAINT fk_voto FOREIGN KEY (IdVoto) REFERENCES VOTO(IdVoto));
 
-CREATE TABLE RestaurantexPlatillo(
+CREATE TABLE VOTOXPLATILLO(
+    IdUniversidad         INTEGER,
     IdRestaurante         INTEGER,
     IdPlatillo            INTEGER,
-    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante(IdRestaurante),
-    CONSTRAINT fk_platillo FOREIGN KEY (IdPlatillo) REFERENCES Platillo(IdPlatillo));
+    IdEstudiante          INTEGER,
+    IdVoto                INTEGER,
+    CONSTRAINT fk_universidad FOREIGN KEY (IdUniversidad) REFERENCES UNIVERSIDAD(IdUniversidad),
+    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES RESTAURANTE(IdRestaurante),
+    CONSTRAINT fk_platillo FOREIGN KEY (IdPlatillo) REFERENCES PLATILLO(IdPlatillo),
+    CONSTRAINT fk_estudiante FOREIGN KEY (IdEstudiante) REFERENCES ESTUDIANTE(IdEstudiante),
+    CONSTRAINT fk_voto FOREIGN KEY (IdVoto) REFERENCES VOTO(IdVoto));
 
-CREATE TABLE MenuxPlatillo(
+CREATE TABLE RESTAURANTEXPLATILLO(
+    IdRestaurante         INTEGER,
+    IdPlatillo            INTEGER,
+    CONSTRAINT fk_restaurante FOREIGN KEY (IdRestaurante) REFERENCES RESTAURANTE(IdRestaurante),
+    CONSTRAINT fk_platillo FOREIGN KEY (IdPlatillo) REFERENCES PLATILLO(IdPlatillo));
+
+CREATE TABLE MENUXPLATILLO(
     IdMenu                INTEGER,
     IdPlatillo            INTEGER,
-    CONSTRAINT fk_menu FOREIGN KEY (IdMenu) REFERENCES Menu(IdMenu),
-    CONSTRAINT fk_platillo FOREIGN KEY (IdPlatillo) REFERENCES Platillo(IdPlatillo));
+    CONSTRAINT fk_menu FOREIGN KEY (IdMenu) REFERENCES MENU(IdMenu),
+    CONSTRAINT fk_platillo FOREIGN KEY (IdPlatillo) REFERENCES PLATILLO(IdPlatillo));
 
 
-INSERT INTO Universidad (Nombre)
+INSERT INTO UNIVERSIDAD (Nombre)
 VALUES ('TEC');
 
-INSERT INTO Calificacion(UpVotes, DownVotes)
+INSERT INTO CALIFICACION(UpVotes, DownVotes)
 VALUES (50, 2),(10,25),(60,30);
 
-UPDATE Universidad SET IdCalificacion = 1 WHERE IdUniversidad = 6;
+UPDATE UNIVERSIDAD SET IdCalificacion = 1 WHERE IdUniversidad = 1;
 
-INSERT INTO Ubicacion (Pais, Provincia, Ciudad, Calle, Direccion)
+INSERT INTO UBICACION (Pais, Provincia, Ciudad, Calle, Direccion)
 VALUES ('Costa Rica', 'Cartago', 'Cartago', 'Calle 15, Avenida 14', '1 km Sur de la Basílica de los Ángeles.'),
     ('Costa Rica', 'Alajuela', 'San Carlos', 'Carretera a Fortuna', '18 kilómetros al norte de Ciudad Quesada.'),
     ('Costa Rica', 'San José', 'San José', 'Calles 5 y 7, Avenida 9 y 11.', 'Barrio Amón'),
     ('Costa Rica', 'San José', 'San Pedro', 'Calles 59.', '3 kilómetros al este del centro de San José');
 
 
-INSERT INTO Sede (Nombre, IdUbicacion, IdUniversidad)
+INSERT INTO SEDE (Nombre, IdUbicacion, IdUniversidad)
 VALUES ('Sede Central',1,1),
     ('Sede Regional San Carlos',2,1),
-    ('Centro Académico San José',3,2);
+    ('Centro Académico San José',3,1);
 
-INSERT INTO Estudiante (Nombre, Apellidos, Carrera, FechaNacimiento, LugarNacimiento, IdUniversidad)
-VALUES ('Alejandro','Calvo Porras', 'Computadores', '03-06-1997', 1, 1),
-    ('Erick','Muñoz Alvarado', 'Computadores', '03-10-1998', 3, 1),
-    ('Fulanito', 'De Tal', 'Admin', '02-05-1992', 4, 1);
+INSERT INTO ESTUDIANTE (Nombre, Apellidos, Carrera, FechaNacimiento, LugarNacimiento, IdUniversidad)
+VALUES ('Alejandro','Calvo Porras', 'Computadores', '1997-06-03', 1, 1),
+    ('Erick','Muñoz Alvarado', 'Computadores', '1998-10-03', 3, 1),
+    ('Fulanito', 'De Tal', 'Admin', '1992-05-02', 4, 1);
 
-INSERT INTO Restaurante (Nombre, Horario, IdSede, IdCalificacion)
+INSERT INTO RESTAURANTE (Nombre, Horario, IdSede, IdCalificacion)
 VALUES ('Comedor','8:00-19:00', 1, 3);
 
-INSERT INTO Menu (IdRestaurante, Horario, Fecha)
-VALUES (1, 'Mañana', '04-07-2018'),
-    (1, 'Mañana', '05-07-2018'),
-    (1, 'Mañana', '06-07-2018'),
-    (1, 'Mañana', '14-07-2018'),
-    (1, 'Mañana', '15-07-2018'),
-    (1, 'Mañana', '16-07-2018'),
-    (1, 'Mañana', '04-08-2018'),
-    (1, 'Mañana', '05-08-2018'),
-    (1, 'Mañana', '06-08-2018'),
-    (1, 'Tarde', '04-07-2018'),
-    (1, 'Tarde', '05-07-2018'),
-    (1, 'Tarde', '06-07-2018'),
-    (1, 'Tarde', '14-07-2018'),
-    (1, 'Tarde', '15-07-2018'),
-    (1, 'Tarde', '16-07-2018'),
-    (1, 'Tarde', '04-08-2018'),
-    (1, 'Tarde', '05-08-2018'),
-    (1, 'Tarde', '06-08-2018');
+INSERT INTO MENU (IdRestaurante, Horario, Fecha)
+VALUES (1, 'Mañana', '2018-07-04'),
+    (1, 'Mañana', '2018-07-05'),
+    (1, 'Mañana', '2018-07-06'),
+    (1, 'Mañana', '2018-07-14'),
+    (1, 'Mañana', '2018-07-15'),
+    (1, 'Mañana', '2018-07-16'),
+    (1, 'Mañana', '2018-04-08'),
+    (1, 'Mañana', '2018-08-05'),
+    (1, 'Mañana', '2018-08-06'),
+    (1, 'Tarde', '2018-07-04'),
+    (1, 'Tarde', '2018-07-05'),
+    (1, 'Tarde', '2018-07-06'),
+    (1, 'Tarde', '2018-07-14'),
+    (1, 'Tarde', '2018-07-15'),
+    (1, 'Tarde', '2018-07-16'),
+    (1, 'Tarde', '2018-04-08'),
+    (1, 'Tarde', '2018-08-05'),
+    (1, 'Tarde', '2018-08-06');
 
 
 Insert INTO PLATILLO (Nombre, Precio)
@@ -202,16 +202,16 @@ VALUES
     (18,5),(18,6),(18,10),(18,9);
     
 INSERT INTO VOTO (Hora, Fecha, Calificacion)
-VALUES ('8:30','04-07-2018',1),
-    ('8:45','04-07-2018',0),
-    ('9:20','04-07-2018',1),
-    ('9:32','04-07-2018',1),
-    ('10:10','06-07-2018',1),
-    ('8:25','16-07-2018',1),
-    ('9:13','16-07-2018',0),
-    ('11:20','05-08-2018',1),
-    ('11:45','05-08-2018',1),
-    ('12:00','05-08-2018',1);
+VALUES ('8:30','2018-07-04',1),
+    ('8:45','2018-07-04',0),
+    ('9:20','2018-07-04',1),
+    ('9:32','2018-07-04',1),
+    ('10:10','2018-07-06',1),
+    ('8:25','2018-07-16',1),
+    ('9:13','2018-07-16',0),
+    ('11:20','2018-08-05',1),
+    ('11:45','2018-08-05',1),
+    ('12:00','2018-08-05',1);
     
 INSERT INTO VOTOXPLATILLO (IdUniversidad, IdRestaurante, IdPlatillo, IdEstudiante, IdVoto)
 VALUES (1,1,1,1,1),
@@ -224,3 +224,17 @@ VALUES (1,1,1,1,1),
     (1,1,8,1,8),
     (1,1,8,2,9),
     (1,1,8,3,10);
+    
+--Se calcula la frecuencia de cada carrera por restaurante
+SELECT VISITXCARRERA.Carrera AS Carrera, VISITXCARRERA.Restaurante AS Restaurante, MAX(VISITXCARRERA.VISITS) VISITS
+FROM (
+  SELECT Estudiante.Carrera AS Carrera, Restaurante.Nombre AS Restaurante, COUNT(ESTUDIANTE.Carrera) VISITS
+  FROM VOTOXPLATILLO
+  INNER JOIN VOTO ON VOTOXPLATILLO.IdVoto = VOTO.IdVoto
+  INNER JOIN ESTUDIANTE ON VOTOXPLATILLO.IdEstudiante = ESTUDIANTE.IdEstudiante
+  INNER JOIN RESTAURANTE ON VOTOXPLATILLO.IdRestaurante = RESTAURANTE.IdRestaurante
+  
+  GROUP BY Carrera, Restaurante
+) AS VISITXCARRERA
+GROUP BY VISITXCARRERA.RESTAURANTE;
+
